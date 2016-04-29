2c2cab73fb2fce3b999c0c4e5d0bc0712746a3a7
Karl Ranna <karl@karlranna.com>
2016-04-29 17:52:16 +0300

    Use reusable NumberInput component

--- src/components/number-input/number-input.js
+++ src/components/number-input/number-input.js
@@ -1,6 +1,38 @@
-const numberInput = (sources) => {
-  console.log('yo');
-  return 0;
+import {Observable} from 'rx'
+import {label, span, section, h1, div, input, makeDOMDriver} from '@cycle/DOM'
+
+function NumberInput({DOM, props$}) {
+  let initialValue$ = props$.map(props => props.initial).first();
+
+  let newValue$ = DOM.select('.number-input')
+    .events('input')
+    .map(ev => {
+      let value = parseFloat(ev.target.value);
+      if(isNaN(value)){
+        return 0;
+      } else {
+        return value;
+      }
+    });
+
+  let value$ = initialValue$.concat(newValue$);
+
+  let vtree$ = Observable.combineLatest(props$, value$, (props, value) =>
+    div('.number-input-container', [
+      div('.mdl-textfield .mdl-js-textfield .mdl-textfield--floating-label', [
+        input(
+          '.number-input .mdl-textfield__input',
+          {type: 'text', value: value, pattern: "-?[0-9]*(\.[0-9]+)?"}
+        ),
+        label('.number-input-label .mdl-textfield__label', props.label),
+      ])
+    ])
+  );
+
+  return {
+    DOM: vtree$,
+    value$
+  };
 };
 
-export default numberInput;
+export default NumberInput;
--- src/main.js
+++ src/main.js
@@ -1,57 +1,43 @@
-import Cycle from '@cycle/core'
-import {Observable} from 'rx'
-import {label, span, section, h1, div, input, makeDOMDriver} from '@cycle/DOM'
 import 'material-design-lite/material.min.css'
 import 'material-design-lite/material.min.js'
-import numberInput from './components/number-input/number-input.js'
+
+import Cycle from '@cycle/core'
+import isolate from '@cycle/isolate'
+import {Observable} from 'rx'
+import {label, span, section, h1, div, input, makeDOMDriver} from '@cycle/DOM'
+import NumberInput from './components/number-input/number-input'
 
 function main({DOM}) {
   let DOMupdated$ = DOM.select(':root').observable.take(1);
-  let changeHourlyRate$ = DOM.select('.hourly-rate')
-    .events('input')
-    .map(ev => {
-      let value = parseFloat(ev.target.value);
-      if(isNaN(value)){
-        return 0;
-      } else {
-        return value;
-      }
-    });
-  let changeHoursWorked$ = DOM.select('.hours-worked')
-    .events('input')
-    .map(ev => {
-      let value = parseFloat(ev.target.value);
-      if(isNaN(value)){
-        return 0;
-      } else {
-        return value;
-      }
-    });
-  let minutesWorked$ = DOM.select('.minutes-worked')
-    .events('input')
-    .map(ev => {
-      let value = parseFloat(ev.target.value);
-      if(isNaN(value)){
-        return 0;
-      } else {
-        return value;
-      }
-    });
-  let secondsWorked$ = DOM.select('.seconds-worked')
-    .events('input')
-    .map(ev => {
-      let value = parseFloat(ev.target.value);
-      if(isNaN(value)){
-        return 0;
-      } else {
-        return value;
-      }
-    });
+
+  let HourlyRateInput = isolate(NumberInput);
+  let HoursInput = isolate(NumberInput);
+  let MinutesInput = isolate(NumberInput);
+  let SecondsInput = isolate(NumberInput);
+
+  let hourlyRateProps$ = Observable.just({
+    label: 'Hourly Rate', initial: 50
+  });
+  let hoursProps$ = Observable.just({
+    label: 'Hours', initial: 40
+  });
+  let minutesProps$ = Observable.just({
+    label: 'Minutes', initial: ''
+  });
+  let secondsProps$ = Observable.just({
+    label: 'Seconds', initial: ''
+  });
+
+  let hourlyRate = HourlyRateInput({DOM, props$: hourlyRateProps$});
+  let hours = HoursInput({DOM, props$: hoursProps$});
+  let minutes = MinutesInput({DOM, props$: minutesProps$});
+  let seconds = SecondsInput({DOM, props$: secondsProps$});
+
   let state$ = Observable.combineLatest(
-    changeHourlyRate$.startWith(50),
-    changeHoursWorked$.startWith(40),
-    minutesWorked$.startWith(0),
-    secondsWorked$.startWith(0),
+    hourlyRate.value$,
+    hours.value$,
+    minutes.value$,
+    seconds.value$,
     (hourlyRate, hoursWorked, minutesWorked, secondsWorked) => {
       let secondsInMinutes = secondsWorked / 60;
       let minutesInHours = (secondsInMinutes + minutesWorked) / 60;
@@ -64,40 +50,29 @@ function main({DOM}) {
       };
 
       secondsWorked = zeroToEmpty(secondsWorked);
-      return {hourlyRate, hoursWorked, minutesWorked, secondsWorked, totalSum: convertedTotalSum};
+      return convertedTotalSum;
     }
   );
+
   return {
-    DOM: state$
-      .map(({hourlyRate, hoursWorked, minutesWorked, secondsWorked, totalSum}) =>
-        div('.container', [
-          section([
-            div('.mdl-textfield .mdl-js-textfield .mdl-textfield--floating-label', [
-              label('.hourly-rate-label', 'Hourly Rate'),
-              input('.hourly-rate', {type: 'text', value: hourlyRate})
-            ])
-          ]),
-          section([
-            div('.mdl-textfield .mdl-js-textfield .mdl-textfield--floating-label', [
-              label('.hours-worked-label', 'Hours'),
-              input('.hours-worked', {type: 'text', value: hoursWorked})
-            ])
-          ]),
-          section([
-            div('.mdl-textfield .mdl-js-textfield .mdl-textfield--floating-label', [
-              label('.minutes-worked-label', 'Minutes'),
-              input('.minutes-worked', {type: 'text', value: minutesWorked})
-            ])
-          ]),
-          div('.mdl-textfield .mdl-js-textfield .mdl-textfield--floating-label', [
-            input('.seconds-worked .mdl-textfield__input #test', {type: 'text', value: secondsWorked, pattern: "-?[0-9]*(\.[0-9]+)?"}),
-            label('.seconds-worked-label .mdl-textfield__label', { attributes: { for: 'test' } }, 'Seconds'),
-          ]),
-          h1('Total: ' + totalSum + '$')
-      ])
-    ),
-  LOG: state$,
-  MDLDriver: DOMupdated$
+    DOM: Observable
+      .combineLatest(
+        state$,
+        hourlyRate.DOM,
+        hours.DOM,
+        minutes.DOM,
+        seconds.DOM,
+        (totalSum, hourlyRateVTree, hoursVTree, minutesVTree, secondsVTree) =>
+          div('.container', [
+            hourlyRateVTree,
+            hoursVTree,
+            minutesVTree,
+            secondsVTree,
+            h1('Total: ' + totalSum + '$')
+          ])
+      ),
+    LOG: state$,
+    MDLDriver: DOMupdated$
   };
 }
 
@@ -109,7 +84,6 @@ Cycle.run(main, {
   },
   LOG: msg$ => { msg$.subscribe(msg => {
       console.log(msg);
-      numberInput();
     });
   }
 })
