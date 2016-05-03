a3e83661e95fc33086a3761c515dc727d6d34277
Karl Ranna <karl@karlranna.com>
2016-05-03 16:31:51 +0300

    Add test ENV configuration

---
+++ dev-server.js
@@ -0,0 +1,25 @@
+var webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
+var config = require('./webpack.config');
+
+// Tests
+var tests = require('./webpack.test.config'),
+  testsCompiler = webpack(tests);
+
+testsCompiler.watch({}, function (err) {
+  if (err) console.log(err);
+});
+
+// Development server
+new WebpackDevServer(webpack(config), {
+  publicPath: config.output.publicPath,
+  hot: true,
+  historyApiFallback: true,
+  stats: {colors: true}
+})
+  .listen(3000, 'localhost', function (err) {
+    if (err) {
+      console.log(err);
+    }
+    console.log('Listening at localhost:3000');
+  });
---
+++ src/__test__/test.spec.js
@@ -0,0 +1,6 @@
+import test from 'tape'
+
+test('it works', function(t){
+  t.plan(1);
+  t.equal(true, false, 'tape + works!');
+});
